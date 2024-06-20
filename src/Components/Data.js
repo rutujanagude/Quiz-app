205f211d4bd650ae4c7c1af3d04f8b11fd954942
Rutuja nagude <rutujanagude7@gmail.com>
2024-06-19 18:03:11 -0700

    Initial commit

---
+++ src/Components/Data.js
@@ -0,0 +1,43 @@
+export const data=[
+    {
+        question:" shortcut keys must be used to start a new task in MS Outlook.",
+        option1:"Ctrl+Shift+K",
+        option2:"Ctrl+Shift+T",
+        option3:"Ctrl+Shift+N",
+        option4:"Ctrl+Shift+L",
+        ans:2,
+    },
+    {
+        question:" Which of the following is not a function of MS Outlook?",
+        option1:"Sending/Receiving Mails",
+        option2:"Organising Meetings and Appointments",
+        option3:"Creating mail thread of similar emails",
+        option4:"All of the Above",
+        ans:4,
+    },
+    {
+        question:"Because Outlook uses Word as its text editor, which of the following featureis NOT identical to Word",
+        option1:"the spelling and grammar checker",
+        option2:"the ability to add SmartArt and WordArt",
+        option3:"the ability to look at people's schedules",
+        option4:"the ability to insert and format tables",
+        ans:3,
+    },
+    {
+        question:" shortcut keys must be used to start a new task in MS Outlook.",
+        option1:"Ctrl+Shift+K",
+        option2:"Ctrl+Shift+T",
+        option3:"Ctrl+Shift+N",
+        option4:"Ctrl+Shift+L",
+        ans:2,
+    },
+    {
+        question:" shortcut keys must be used to start a new task in MS Outlook.",
+        option1:"Ctrl+Shift+K",
+        option2:"Ctrl+Shift+T",
+        option3:"Ctrl+Shift+N",
+        option4:"Ctrl+Shift+L",
+        ans:2,
+    }
+    
+];
